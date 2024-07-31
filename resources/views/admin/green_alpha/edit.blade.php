72e4df4811fadc1c3b8f5753021d66d4b842e75d
nguyentien1201 <nguyentien1201@gmail.com>
2024-07-31 23:06:44 +0700

    update

--- resources/views/admin/green_alpha/edit.blade.php
+++ resources/views/admin/green_alpha/edit.blade.php
@@ -27,9 +27,9 @@
                                             class="form-control custom-select @if($errors->has('code')) is-invalid @endif">
                                         <option value="">{{__('panel.please_choose')}}</option>
                                         @foreach($mstStocks as $mstStock)
-                                            <option value="{{$mstStock->id}}"
-                                            @if($signal->code == $mstStock->id) selected @endif
-                                                    @if(old('code') == $mstStock->id) selected @endif>{{$mstStock->code}}</option>
+                                            <option value="{{$mstStock['id']}}"
+                                            @if($signal->code == $mstStock['id']) selected @endif
+                                                    @if(old('code') == $mstStock['id']) selected @endif>{{$mstStock['code']}}</option>
                                         @endforeach
                                     </select>
                                     <input type="hidden" name="code" value="{{$signal->code}}">
