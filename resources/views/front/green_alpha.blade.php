427759574bad093e60877955a1081cb0f501fbf9
nguyentien1201 <nguyentien1201@gmail.com>
2024-08-03 21:32:40 +0700

    Update green_alpha.blade.php

--- resources/views/front/green_alpha.blade.php
+++ resources/views/front/green_alpha.blade.php
@@ -805,7 +805,7 @@
                     render: function (data, type, row) {
                         if (data == null || data == '') return '';
                         if (type === 'display' || type === 'filter') {
-                            return moment.utc(data).format('YYYY-MM-DD HH:mm'); // Format as HH:mm
+                            return moment.utc(data).format('HH:mm'); // Format as HH:mm
                         }
                         return data;
                     }
@@ -815,7 +815,7 @@
                     render: function (data, type, row) {
                         if (data == null || data == '') return '';
                         if (type === 'display' || type === 'filter') {
-                            return moment.utc(data).format('YYYY-MM-DD HH:mm'); // Format as HH:mm
+                            return moment.utc(data).format('HH:mm'); // Format as HH:mm
                         }
                         return data;
                     }
