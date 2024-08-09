efcf42337cd3c9b2fc51c11f47d4338a443f7798
nguyentien1201 <nguyentien1201@gmail.com>
2024-08-09 23:18:50 +0700

    Update index.blade.php

--- resources/views/admin/nas100/index.blade.php
+++ resources/views/admin/nas100/index.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>{{ __('panel.alpha') }}</h1>
+                    <h1>Green Stock Nas100</h1>
                 </div>
             </div>
         </div>
