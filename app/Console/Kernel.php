39966b1f729cba79484a460cc537244fca9e1294
nguyentien1201 <nguyentien1201@gmail.com>
2024-08-09 23:24:46 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//        $schedule->command('sync:order')->everyMinute()->withoutOverlapping(10);
-        $schedule->command('send:promotion')->everyMinute()->withoutOverlapping(10);
+        $schedule->command('sync:nas100')->dailyAt('23:30')->withoutOverlapping(10);
+        // $schedule->command('send:promotion')->everyMinute()->withoutOverlapping(10);
 //        $schedule->command('send:birthday')->dailyAt('09:30')->withoutOverlapping(10);
     }
 
