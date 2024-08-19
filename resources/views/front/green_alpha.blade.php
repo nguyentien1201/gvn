eb12bfdfbad2f2baeeedc29c59ef3dd7d2d73ebc
nguyentien1201 <nguyentien1201@gmail.com>
2024-08-20 01:12:00 +0700

    Update green_alpha.blade.php

--- resources/views/front/green_alpha.blade.php
+++ resources/views/front/green_alpha.blade.php
@@ -413,7 +413,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
-<script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+    <script src="
+https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
+"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0-rc/dist/chartjs-plugin-datalabels.min.js"></script>
 <script src="https://cdn.datatables.net/v/bs5/dt-2.0.8/date-1.5.2/fc-5.0.1/fh-4.0.1/r-3.0.2/datatables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
 <script>
@@ -708,6 +711,7 @@
                             }]
                         },
                         options: {
+
                             scales: {
                                 x: {
                                     ticks: {
@@ -873,7 +877,6 @@
     });
 </script>
 <script>
-    console.log(@json($dataChartProfit));
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
@@ -895,6 +898,21 @@
             }]
         },
         options: {
+            plugins: {
+                datalabels: {
+                    display: true, // Hiển thị giá trị
+                    anchor: 'end',
+                        align: 'end',
+                        formatter: function(value, context) {
+                            return value + '%';
+                        },
+                        labels: {
+                        value: {
+                        color: 'blue'
+                        }
+                    }
+                    }
+            },
             scales: {
                 x: {
                     ticks: {
@@ -913,7 +931,7 @@
             }
 
         },
-
+        plugins: [ChartDataLabels]
     });
 
 </script>
