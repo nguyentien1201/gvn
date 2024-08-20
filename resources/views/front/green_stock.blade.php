2b519628c76e680e2d779e213088c65a6607fd2f
nguyentien1201 <nguyentien1201@gmail.com>
2024-08-20 07:23:14 +0700

    Update green_stock.blade.php

--- resources/views/front/green_stock.blade.php
+++ resources/views/front/green_stock.blade.php
@@ -285,7 +285,7 @@
                 /* Đặt cột chính ở vị trí cuối cùng */
             }
         }
-        @media (min-width: 768px) and (max-width: 1280px) {
+        @media (min-width: 768px) {
             .container_layout {
                 flex-direction: row;
                 /* Xếp ngang các phần tử */
