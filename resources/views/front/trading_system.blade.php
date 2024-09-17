9decbf442b5813df6d740a33425f7b3158d4582b
nguyentien1201 <nguyentien1201@gmail.com>
2024-09-17 15:26:45 +0700

    Update trading_system.blade.php

--- resources/views/front/trading_system.blade.php
+++ resources/views/front/trading_system.blade.php
@@ -144,7 +144,7 @@ https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
         .pricing-table {
             display: flex;
             justify-content: center;  /* Căn giữa theo chiều ngang */
-            gap: 20px;
+
         }
         .basic {
             background-color: #4f4fee;
@@ -252,6 +252,222 @@ https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
             font-weight: bold;
             margin-right: 10px;
         }
+
+
+
+
+        .pricing-table {
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  background: #ffffff;
+}
+
+.pricing-table .ptable-item {
+
+  padding: 0 15px;
+  margin-bottom: 30px;
+}
+
+
+
+.pricing-table .ptable-single {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+
+.pricing-table .ptable-header,
+.pricing-table .ptable-body,
+.pricing-table .ptable-footer {
+  position: relative;
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
+}
+
+.pricing-table .ptable-status ,
+.pricing-table .ptable-title,
+.pricing-table .ptable-price,
+.pricing-table .ptable-description,
+.pricing-table .ptable-action {
+  position: relative;
+  width: 100%;
+  text-align: center;
+}
+
+.pricing-table .ptable-single {
+  background: #f6f8fa;
+}
+
+.pricing-table .ptable-single:hover {
+  box-shadow: 0 0 10px #999999;
+}
+
+.pricing-table .ptable-header {
+  margin: 0 30px;
+  padding: 30px 0 45px 0;
+  width: auto;
+  background: #4f4fee;
+}
+
+.pricing-table .ptable-header::before,
+.pricing-table .ptable-header::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-bottom: 100px solid #f6f8fa;
+}
+
+.pricing-table .ptable-header::before {
+  right: 50%;
+  border-right: 250px solid transparent;
+}
+
+.pricing-table .ptable-header::after {
+  left: 50%;
+  border-left: 250px solid transparent;
+}
+
+.pricing-table .ptable-item.featured-item .ptable-header {
+  background: #4caf50;
+}
+
+.pricing-table .ptable-item.premium-item .ptable-header {
+  background: #f44336;
+}
+.pricing-table .ptable-status {
+  margin-top: -30px;
+}
+
+.pricing-table .ptable-status span {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 30px;
+  padding: 5px 0;
+  text-align: center;
+  color: #4caf50;
+  font-size: 14px;
+  font-weight: 300;
+  letter-spacing: 1px;
+  background: #FFD700;
+}
+
+.pricing-table .ptable-status span::before,
+.pricing-table .ptable-status span::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-bottom: 10px solid #4caf50;
+}
+
+.pricing-table .ptable-status span::before {
+  right: 50%;
+  border-right: 25px solid transparent;
+}
+
+.pricing-table .ptable-status span::after {
+  left: 50%;
+  border-left: 25px solid transparent;
+}
+
+.pricing-table .ptable-title h2 {
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 300;
+  letter-spacing: 2px;
+}
+
+.pricing-table .ptable-price h2 {
+  margin: 0;
+  color: #ffffff;
+  font-size: 45px;
+  font-weight: 700;
+  margin-left: 15px;
+}
+
+.pricing-table .ptable-price h2 small {
+  position: absolute;
+  font-size: 18px;
+  font-weight: 300;
+  margin-top: 16px;
+  margin-left: -15px;
+}
+
+.pricing-table .ptable-price h2 span {
+  margin-left: 3px;
+  font-size: 16px;
+  font-weight: 300;
+}
+
+.pricing-table .ptable-body {
+  padding: 20px 0;
+}
+
+.pricing-table .ptable-description ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.pricing-table .ptable-description ul li {
+  color: #2A293E;
+  font-size: 14px;
+  font-weight: 300;
+  letter-spacing: 1px;
+  padding: 7px;
+  border-bottom: 1px solid #dedede;
+}
+
+.pricing-table .ptable-description ul li:last-child {
+  border: none;
+}
+
+.pricing-table .ptable-footer {
+  padding-bottom: 30px;
+}
+
+.pricing-table .ptable-action a {
+  display: inline-block;
+  padding: 10px 20px;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 2px;
+  text-decoration: none;
+  background: #0d6efd;
+}
+
+.pricing-table .ptable-action a:hover {
+  color: white;
+  background: #0d6efd;
+}
+
+.pricing-table .ptable-item.featured-item .ptable-action a {
+  color: white;
+  background: #4caf50;
+}
+
+.pricing-table .ptable-item.featured-item .ptable-action a:hover {
+  color: white;
+  background: #4caf50;
+}
+.pricing-table .ptable-item.premium-item .ptable-action a {
+  color: white;
+  background: #f44336;
+}
+
+.pricing-table .ptable-item.premium-item .ptable-action a:hover {
+  color: white;
+  background: #f44336;
+}
     </style>
 
 </head>
@@ -293,40 +509,90 @@ https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
                     cá mập trên thị trường toàn cầu.</h4>
             </div>
         </div>
-        <div class="container pb-3" style="background: #f9f9f9;">
+        <div class="container pb-3 mt-5" style="background: #f9f9f9;">
             <div class="row">
-                <div class="col-md-12 text-center price_block">
-                    <h2 class="text-center mt-5">PRICE TABLE TEMPLATE</h2>
-                    <div class=" pricing-table mt-3">
-                    <div class="pricing-card">
-                        <h3>1 Tháng</h3>
-                        <p class="price basic">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-
-                        </ul>
-                        <a href="#" class="btn basic">Buy Now</a>
+            <div class="pricing-table">
+            <div class="ptable-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <div class="ptable-title">
+                    <h2>1 Tháng</h2>
                     </div>
-
-                    <div class="pricing-card">
-                        <h3>3 Tháng</h3>
-                        <p class="price standard">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-                        </ul>
-                        <a href="#" class="btn standard">Buy Now</a>
+                    <div class="ptable-price basic">
+                    <h2><small>$</small>0<span>/ M</span></h2>
                     </div>
-
-                    <div class="pricing-card">
-                        <h3>3 Tháng</h3>
-                        <p class="price premium">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-                        </ul>
-                        <a href="#" class="btn premium">Buy Now</a>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                    <li>Phiên Bản Dùng Thử</li>
+                    <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
                     </div>
                 </div>
                 </div>
+            </div>
+
+            <div class="ptable-item featured-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <!-- <div class="ptable-status">
+                    <span>Hot</span>
+                    </div> -->
+                    <div class="ptable-title">
+                    <h2>6 Tháng</h2>
+                    </div>
+                    <div class="ptable-price">
+                    <h2><small>$</small>0<span>/ M</span></h2>
+                    </div>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                        <li>Phiên Bản Dùng Thử</li>
+                        <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="ptable-item premium-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <div class="ptable-title">
+                    <h2>1 năm</h2>
+                    </div>
+                    <div class="ptable-price">
+                    <h2><small>$</small>0<span>/ M</span></h2>
+                    </div>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                        <li>Phiên Bản Dùng Thử</li>
+                        <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
 
             </div>
         </div>
@@ -358,40 +624,90 @@ https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
 
             </div>
         </div>
-        <div class="container pb-3" style="background: #f9f9f9;">
+        <div class="container pb-3 mt-5" style="background: #f9f9f9;">
             <div class="row">
-                <div class="col-md-12 text-center price_block">
-
-                    <div class=" pricing-table mt-3">
-                    <div class="pricing-card">
-                        <h3>1 Tháng</h3>
-                        <p class="price basic">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-
-                        </ul>
-                        <a href="#" class="btn basic">Buy Now</a>
+            <div class="pricing-table">
+            <div class="ptable-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <div class="ptable-title">
+                    <h2>1 Tháng</h2>
                     </div>
-
-                    <div class="pricing-card">
-                        <h3>6 Tháng</h3>
-                        <p class="price standard">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-                        </ul>
-                        <a href="#" class="btn standard">Buy Now</a>
+                    <div class="ptable-price basic">
+                    <h2><small>$</small>0<span>/ M</span></h2>
                     </div>
-
-                    <div class="pricing-card">
-                        <h3>1 Năm</h3>
-                        <p class="price premium">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-                        </ul>
-                        <a href="#" class="btn premium">Buy Now</a>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                    <li>Phiên Bản Dùng Thử</li>
+                    <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
                     </div>
                 </div>
                 </div>
+            </div>
+
+            <div class="ptable-item featured-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <!-- <div class="ptable-status">
+                    <span>Hot</span>
+                    </div> -->
+                    <div class="ptable-title">
+                    <h2>6 Tháng</h2>
+                    </div>
+                    <div class="ptable-price">
+                    <h2><small>$</small>0<span>/ M</span></h2>
+                    </div>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                        <li>Phiên Bản Dùng Thử</li>
+                        <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="ptable-item premium-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <div class="ptable-title">
+                    <h2>1 năm</h2>
+                    </div>
+                    <div class="ptable-price">
+                    <h2><small>$</small>0<span>/ M</span></h2>
+                    </div>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                        <li>Phiên Bản Dùng Thử</li>
+                        <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
 
             </div>
         </div>
@@ -423,40 +739,90 @@ https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js
 
             </div>
         </div>
-        <div class="container pb-3" style="background: #f9f9f9;">
+        <div class="container pb-3 mt-5" style="background: #f9f9f9;">
             <div class="row">
-                <div class="col-md-12 text-center price_block">
-                    <h2 class="text-center mt-5">PRICE TABLE TEMPLATE</h2>
-                    <div class=" pricing-table mt-3">
-                    <div class="pricing-card">
-                        <h3>1 Tháng</h3>
-                        <p class="price basic">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-
-                        </ul>
-                        <a href="#" class="btn basic">Buy Now</a>
+            <div class="pricing-table">
+            <div class="ptable-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <div class="ptable-title">
+                    <h2>1 Tháng</h2>
                     </div>
-
-                    <div class="pricing-card">
-                        <h3>3 Tháng</h3>
-                        <p class="price standard">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-                        </ul>
-                        <a href="#" class="btn standard">Buy Now</a>
+                    <div class="ptable-price basic">
+                    <h2><small>$</small>0<span>/ M</span></h2>
                     </div>
-
-                    <div class="pricing-card">
-                        <h3>3 Tháng</h3>
-                        <p class="price premium">$0</p>
-                        <ul style="min-height:120px">
-                            <li><i class="fas fa-check tick"></i>Phiên bản dùng thử</li>
-                        </ul>
-                        <a href="#" class="btn premium">Buy Now</a>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                    <li>Phiên Bản Dùng Thử</li>
+                    <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
                     </div>
                 </div>
                 </div>
+            </div>
+
+            <div class="ptable-item featured-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <!-- <div class="ptable-status">
+                    <span>Hot</span>
+                    </div> -->
+                    <div class="ptable-title">
+                    <h2>6 Tháng</h2>
+                    </div>
+                    <div class="ptable-price">
+                    <h2><small>$</small>0<span>/ M</span></h2>
+                    </div>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                        <li>Phiên Bản Dùng Thử</li>
+                        <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="ptable-item premium-item col-12 col-sm-12 col-md-6 col-lg-4">
+                <div class="ptable-single">
+                <div class="ptable-header">
+                    <div class="ptable-title">
+                    <h2>1 năm</h2>
+                    </div>
+                    <div class="ptable-price">
+                    <h2><small>$</small>0<span>/ M</span></h2>
+                    </div>
+                </div>
+                <div class="ptable-body">
+                    <div class="ptable-description">
+                    <ul>
+                        <li>Phiên Bản Dùng Thử</li>
+                        <li>Thời hạn 1 tháng</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="ptable-footer">
+                    <div class="ptable-action">
+                    <a href="">Buy Now</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
 
             </div>
         </div>
