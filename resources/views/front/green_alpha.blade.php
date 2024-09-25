12ff9630d70adcc6a26f1f837fb6abbb894b6f46
nguyentien1201 <nguyentien1201@gmail.com>
2024-09-25 07:16:16 +0700

    Update green_alpha.blade.php

--- resources/views/front/green_alpha.blade.php
+++ resources/views/front/green_alpha.blade.php
@@ -738,7 +738,7 @@
                     render: function (data, type, row) {
                         if (data == null || data == '') return '';
                         if (type === 'display' || type === 'filter') {
-                            return moment(data).format('HH:mm'); // Format as HH:mm
+                            return  moment.tz(data, 'Europe/Moscow').format('HH:mm'); // Format as HH:mm
                         }
                         return data;
                     }
@@ -748,7 +748,7 @@
                     render: function (data, type, row) {
                         if (data == null || data == '') return '';
                         if (type === 'display' || type === 'filter') {
-                            return moment(data).format('HH:mm'); // Format as HH:mm
+                            return  moment.tz(data, 'Europe/Moscow').format('HH:mm'); // Format as HH:mm
                         }
                         return data;
                     }
