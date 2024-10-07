d43dbddc3098f0d87801f2569c62ae26c78b2baa
nguyentien1201 <nguyentien1201@gmail.com>
2024-10-07 16:03:01 +0700

    Update mail-contact.blade.php

--- resources/views/front/common/mail-contact.blade.php
+++ resources/views/front/common/mail-contact.blade.php
@@ -1,5 +1,6 @@
 <h1>Contact GVN</h1>
 <p><strong>Name:</strong> {{ $name }}</p>
 <p><strong>Email:</strong> {{ $email }}</p>
+<p><strong>Product:</strong> {{ $product }}</p>
 <p><strong>Message:</strong></p>
 <p>{{ $note }}</p>
