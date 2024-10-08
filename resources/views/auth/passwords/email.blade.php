e2a662be17ed797fe01875f608af29849a87a4e9
nguyentien1201 <nguyentien1201@gmail.com>
2024-10-08 09:41:46 +0700

    a

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -28,6 +28,8 @@
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <!-- DataTables Responsive CSS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 @include('front.common.header')
 
 <div class="container mt-5">
