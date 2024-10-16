486e1ff1cdc30ce146767bc987f71373246e27ae
nguyentien1201 <nguyentien1201@gmail.com>
2024-10-16 10:35:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::group(['prefix' => '', 'as' => 'front.', 'namespace' => 'Front'], functio
     Route::get('api/get-history-alpha/{id}', 'HomeController@getHistoryAlphaSignal')->name('api.history-signal-alpha');
     Route::get('/greenstock-nas100', 'HomeController@greenStock')->name('home.green-stock')->middleware(['auth']);
     Route::get('api/get-market-greenstock', 'HomeController@getMarketGreenStock')->name('api.market-greenstock');
-    Route::get('api/get-top-stock', 'HomeController@getTopStock')->name('api.top-stock');
+    Route::get('api/get-top-stock', 'HomeController@getTopStock')->middleware(['auth'])->name('api.top-stock');
     Route::get('trading-system', 'HomeController@tradingSystem')->name('home.trading-system');
     Route::get('contact', 'HomeController@contact')->name('home.contact');
     Route::get('mission', 'HomeController@mission')->name('home.mission');
