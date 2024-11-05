5f1ca3226066b2bcff8166abb25be53f03e242ce
nguyentien1201 <nguyentien1201@gmail.com>
2024-11-05 07:32:16 +0700

    Update ApiController.php

--- app/Http/Controllers/Front/ApiController.php
+++ app/Http/Controllers/Front/ApiController.php
@@ -40,9 +40,15 @@ class ApiController
                 'price_close'=> $signal[2],
                 'close_time'=> $timeFormat,
             ];
+
             $existSignal = GreenAlpha::where('code', $listCode[$signal[0]])->whereNull('close_time')->whereDate('open_time', '=', $signal[3])->first();
             if(empty($existSignal)) return  ['status' => 'error', 'message' => 'No signal recived'];
-            $profit = (float)$signalData['price_close'] - (float)$existSignal->price_open;
+            $profit =0;
+            if($existSignal->signal_open =='BUY'){
+                $profit = (float)$signalData['price_close'] - (float)$existSignal->price_open;
+            }else{
+                $profit = (float)$existSignal->price_open - (float)$signalData['price_close'] ;
+            }
 
             $existSignal->update($signalData);
             $message = "<b>GREEN ALPHA(Ver 10.5)</b>\nSymbol: <b>".$signal[0]."</b>\nSignal: <b>".$signalData['signal_close']."</b>\nPrice Close: <b>".$signalData['price_close']."</b>\n"."Profit: <b>".round($profit, 2)." pts</b>"."\nTime: <b>".$timeSendTelegram."</b>";
