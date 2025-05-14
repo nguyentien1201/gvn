e19c4d785520d4152f743296d7bdd63db4fd35cd
nguyentien1201 <nguyentien1201@gmail.com>
2025-05-15 06:28:45 +0700

    no message

--- resources/views/frontend_v2/contact.blade.php
+++ resources/views/frontend_v2/contact.blade.php
@@ -3,12 +3,10 @@
 @push('styles')
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <!-- App CSS -->
     <link rel="stylesheet" href="{{ asset('css/mission.css') }}">
+
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
     <style>
          .card-icon {
