b92435f6cbf56db7939b56510b073facf924ba1b
nhoxnow01 <thienvucdtt6@gmail.com>
2025-05-18 22:29:12 +0700

    Coding timeline on mobile and tablet

--- resources/views/frontend_v2/mission.blade.php
+++ resources/views/frontend_v2/mission.blade.php
@@ -33,9 +33,8 @@
             <hr>
             <h3 class="text-center heading-page my-4">{{__('mission.timeline')}}</h3>
             <div class="timeline-container">
-                <img class="w-100 vector" src="{{asset('images/mission/vector.svg')}}" alt="">
-
                 <!--Time line -->
+                <img class="w-100 vector" src="{{asset('images/mission/vector.svg')}}" alt="">
                 <div class="timeline flex flex-row">
                     @foreach($timelines as $key => $timeline)
                         @if(!($key % 2) || $key == 0)
@@ -92,12 +91,13 @@
                         @endif
                     @endforeach
                 </div>
-                <div class="timeline-mobile d-md-none">
-                    <img class="img-fluid" src="{{asset('images/mission/vector-mobile.svg')}}" alt="">
+                <!--Time line mobile -->
+                <div class="timeline-mobile position-relative">
+                    <div class="timeline-items clearfix">
                     @foreach($timelines as $key => $timeline)
-                        <div class="d-flex flex-row timeline-item">
-                            <div class="timeline-img">
-                                <img class="img-fluid" src="{{asset('images/mission/timeline/'.$timeline['image'])}}"
+                        <div class="d-flex flex-row timeline-item clearfix {{(($key ) != count($timeline)) ? 'mb-5' : ''}}">
+                            <div style="flex-shrink: 0;" class="timeline-img">
+                                <img class="img-fluid object-fit-cover" src="{{asset('images/mission/timeline/'.$timeline['image'])}}"
                                      alt="{{$timeline['timeline_name']}}">
                             </div>
                             <div class="timeline-info d-flex flex-column">
@@ -111,40 +111,42 @@
                             </div>
                         </div>
                     @endforeach
-                </div>
-            </div>
-            <hr>
-            <!--Our Team -->
-            <div id="our-team">
-                <h3 class="text-center heading-page">{{__('mission.our_team')}}</h3>
-                <p class="text-center des-title">{{__('mission.text_title')}}</p>
-                <div class="container-our-team my-3">
-                    <div class="row gy-4">
-                        @for($i = 1; $i <= 8; $i++)
-                            <div class="col-6 col-md-4 col-lg-3">
-                                <div class="member flex flex-column gap-2">
-                                    <img class="img-fluid" src="{{asset('images/mission/member.png')}}"
-                                         alt="{{asset('images/mission/member.png')}}">
-                                    <h3 class="member-name">Darrell Steward</h3>
-                                    <h4 class="member-position">Chief Technology Officer</h4>
-                                    <p class="member-introduce">20 years in finance, founded GVN to empower investors.
-                                        Ex-Senior Executive at a hedge fund.</p>
-                                </div>
-                            </div>
-                        @endfor
                     </div>
                 </div>
             </div>
         </div>
-        @auth
-            <div style="position: fixed; bottom: 20px; right: 20px; text-align: center; z-index: 1000;">
-                <div class="sc-9qme4p-0 hELAUe">
-                    <button class="decription_telegram"><span
-                                class="sc-1ee9gtf-2 bxZLwE">{{__('front_end.chat_with_me')}}</span></button>
+        <hr>
+        <!--Our Team -->
+        <div id="our-team">
+            <h3 class="text-center heading-page">{{__('mission.our_team')}}</h3>
+            <p class="text-center des-title">{{__('mission.text_title')}}</p>
+            <div class="container-our-team container container-xxl my-3">
+                <div class="row gy-4">
+                    @for($i = 1; $i <= 8; $i++)
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <div class="member flex flex-column gap-2">
+                                <img class="img-fluid" src="{{asset('images/mission/member.png')}}"
+                                     alt="{{asset('images/mission/member.png')}}">
+                                <h3 class="member-name">Darrell Steward</h3>
+                                <h4 class="member-position">Chief Technology Officer</h4>
+                                <p class="member-introduce">20 years in finance, founded GVN to empower investors.
+                                    Ex-Senior Executive at a hedge fund.</p>
+                            </div>
+                        </div>
+                    @endfor
                 </div>
-                <a href="https://t.me/{{config('config.telegram_user')}}" target="_blank"
-                   style="text-decoration: none;">
-                    <button style="
+            </div>
+        </div>
+    </div>
+    @auth
+        <div style="position: fixed; bottom: 20px; right: 20px; text-align: center; z-index: 1000;">
+            <div class="sc-9qme4p-0 hELAUe">
+                <button class="decription_telegram"><span
+                            class="sc-1ee9gtf-2 bxZLwE">{{__('front_end.chat_with_me')}}</span></button>
+            </div>
+            <a href="https://t.me/{{config('config.telegram_user')}}" target="_blank"
+               style="text-decoration: none;">
+                <button style="
         float: right;
       background-color: #33a853;
       color: white;
@@ -159,15 +161,15 @@
       cursor: pointer;
       transition: background-color 0.3s ease;
     "
-                            onmouseover="this.style.backgroundColor='#33a853';"
-                            onmouseout="this.style.backgroundColor='#198754';">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" alt="Telegram"
-                             style="width: 30px; height: 30px;">
-                    </button>
-                </a>
-            </div>
+                        onmouseover="this.style.backgroundColor='#33a853';"
+                        onmouseout="this.style.backgroundColor='#198754';">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" alt="Telegram"
+                         style="width: 30px; height: 30px;">
+                </button>
+            </a>
+        </div>
         @endauth
         @include('frontend_v2.components.footer')
 
-    </div>
+        </div>
 @endsection
