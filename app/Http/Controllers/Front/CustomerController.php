c6604b3d7bd3cb1145a74643333e7f224a67a208
nguyentien1201 <nguyentien1201@gmail.com>
2025-05-22 07:59:29 +0700

    Update CustomerController.php

--- app/Http/Controllers/Front/CustomerController.php
+++ app/Http/Controllers/Front/CustomerController.php
@@ -1,9 +1,21 @@
+<?php
+namespace App\Http\Controllers\Front;
+
+use App\Models\Product;
+use App\Models\Subscription;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\ConstantModel;
+use Carbon\Carbon;
+use App\Models\Profile;
 class CustomerController
 {
     public function myAccount(){
         $subscriptions = (new Subscription())->getMySubscription();
+
         $info = User::with(['profile'])->where('id', Auth::id())->select('id', 'email',  'name')->first();
-        return view('front.my_account', compact('subscriptions','info'));
+        return view('frontend_v2.my_account', compact('subscriptions','info'));
     }
     public function update( Profile $profile ,Request $request)
     {
