e414954f761e38a71c308b67eb7db975c0439997
nguyentien1201 <nguyentien1201@gmail.com>
2025-06-06 23:00:44 +0700

    Update my_account.blade.php

--- resources/views/frontend_v2/my_account.blade.php
+++ resources/views/frontend_v2/my_account.blade.php
@@ -453,10 +453,7 @@
 
                             <div class="d-flex justify-content-end mt-3">
                                 <ul class="pagination">
-                                    <li class="page-item disabled"><a class="page-link">«</a></li>
-                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">»</a></li>
+
                                 </ul>
                             </div>
                         </div>
