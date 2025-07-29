3b96603e7eec2561c580ed30571b1315dc315320
TienNguyen <nguyentien1201@gmail.com>
2025-07-29 12:02:37 +0900

    add

--- resources/views/frontend_v2/green_beta.blade.php
+++ resources/views/frontend_v2/green_beta.blade.php
@@ -193,6 +193,7 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0-rc/dist/chartjs-plugin-datalabels.min.js"></script>
 <script>
+        window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
