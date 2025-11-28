e80695017378a28f9567aa3303ab29c8f65182eb
nguyentien1201 <nguyentien1201@gmail.com>
2025-11-28 20:22:42 +0700

    no message

--- resources/views/frontend_v2/investment.blade.php
+++ resources/views/frontend_v2/investment.blade.php
@@ -99,14 +99,15 @@
                                     </td>
                                     <td class="text-center align-middle">{{ $invest->name }}</td>
 
-                                    <td class="text-center align-middle">{{number_format($invest->total_value, 0, ',', '.')}}</td>
+                                    <td class="text-center align-middle">{{ $invest->take_profit_price }}</td>
+                                    <td class="text-center align-middle">{{number_format($invest->total_value, 0, ',', '.') }}</td>
                                     <td class="text-center align-middle">{{number_format($invest->total_shares, 0, ',', '.')}}</td>
                                     <td class="text-center align-middle">{{ $invest->avg_price }}</td>
                                     <td class="text-center align-middle">{{ $invest->invest_date }}</td>
                                     <td class="text-center align-middle">{{ $invest->current_price }}</td>
-                                    <td class="text-center align-middle" style="font-weight: 500; color: {{ $invest->current_profit_percent > 0 ? 'green' : ($invest->current_profit_percent < 0 ? 'red' : 'inherit') }};">{{ $invest->current_profit_percent }}</td>
+                                    <td class="text-center align-middle" style="font-weight: 500; color: {{ $invest->current_profit_percent > 0 ? 'green' : ($invest->current_profit_percent < 0 ? 'red' : 'inherit') }};">{{ $invest->current_profit_percent }}%</td>
                                     <td class="text-center align-middle">{{ $invest->take_profit_price }}</td>
-                                    <td class="text-center align-middle" style="font-weight: 500; color: {{ $invest->take_profit_percent > 0 ? 'green' : ($invest->take_profit_percent < 0 ? 'red' : 'inherit') }};">{{ $invest->take_profit_percent }}</td>
+                                    <td class="text-center align-middle" style="font-weight: 500; color: {{ $invest->take_profit_percent > 0 ? 'green' : ($invest->take_profit_percent < 0 ? 'red' : 'inherit') }};">{{ $invest->take_profit_percent }}%</td>
                                     <td class="text-center align-middle">{{ $invest->take_profit_expected }}</td>
                                     <td class="text-center align-middle">
                                         {{ $invest->status == 0 ? 'ĐANG MỞ GỌI VỐN' : ($invest->status == 1 ? 'ĐANG NẮM GIỮ' : 'ĐÃ TẤT TOÁN CHO NHÀ ĐẦU TƯ') }}
