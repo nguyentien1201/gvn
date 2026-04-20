861bae3e25161bc8e58649ad517e125e1220451b
Tien Nguyen <tien.nguyen@dki-sd.com>
2026-04-20 15:37:28 +0700

    Update investment.blade.php

--- resources/views/frontend_v2/investment.blade.php
+++ resources/views/frontend_v2/investment.blade.php
@@ -73,6 +73,8 @@
                 <h5 class="time-live mb-0 text-right">
                     <i><span class="date-js"></span> <span class="time-js"></span> (UTC+7)</i>
                 </h5>
+                <h3 class="heading-page time-live">TỔNG TÀI SẢN ĐẦU TƯ HIỆN TẠI: {{ number_format($total_investment, 0, ',', '.') }}</h3>
+
                 <div class="table-responsive mt-5" >
                     <table id="investment-table" class="table table-striped table-hover" style="margin:none">
                         <thead>
@@ -134,26 +136,7 @@
     </div>
         <section class="py-2">
             <div class="container-fluid">
-                <div class="row">
-                    <div class="col-3 col-md-6 col-lg-4">
-                        <div class="table-responsive mt-5 cot-noi-dung" >
-                            <table class="table table-striped table-hover" style="margin:none">
-                                <thead>
 
-                                    <tr>
-                                        <th>TỔNG TÀI SẢN ĐẦU TƯ HIỆN TẠI</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                     <tr>
-                                            <td>{{ number_format($total_investment, 0, ',', '.') }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-
-                    </div>
-                </div>
                 <div class="row">
                     <div class="col-12 col-md-6 col-lg-4">
                         <div class="table-responsive mt-5 cot-noi-dung" >
